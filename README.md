8710b954612b384eab567a9a7d5b842bfff6bee5
Radmir Sharafutdinov <rsarafutdinov186@icloud.com>
2024-11-05 18:04:08 +0500

    Выполнено задание №9

--- README.md
+++ README.md
@@ -231,3 +231,32 @@ while True:
 
                 
 ```
+## задание 6 ##
+
+```
+import re
+def em(email):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        match = re.fullmatch(pattern, email)
+        if match:
+                group = email.split('@')
+                name = group[0]
+                domain = group[1]
+                return name, domain
+        else:
+                return None, None
+while True:
+        a = input("Введите емаил или stop ")
+        if a =="stop":
+                break
+        name, domain = em(a)
+        if name and domain:
+                print("Имя пользователя: ", name)
+                print("Домен: ", domain)
+                continue
+        else:
+                print("Неверный формат электронной почты")
+                continue
+
+                
+```
--- homework9/1.py
+++ homework9/1.py
@@ -1,6 +1,23 @@
 import re
-a = input("Введите emai")
-if len(re.findall("@", a))==1:
-        print("cool")
-else:
-        print("Некорректный email")
+def em(email):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        match = re.fullmatch(pattern, email)
+        if match:
+                group = email.split('@')
+                name = group[0]
+                domain = group[1]
+                return name, domain
+        else:
+                return None, None
+while True:
+        a = input("Введите емаил или stop ")
+        if a =="stop":
+                break
+        name, domain = em(a)
+        if name and domain:
+                print("Имя пользователя: ", name)
+                print("Домен: ", domain)
+                continue
+        else:
+                print("Неверный формат электронной почты")
+                continue
